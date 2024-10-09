0b07d1b27496fc8c967e42a7d12cd50ff842f48c
Kalhum <129973108+Kalhum@users.noreply.github.com>
2024-10-09 22:16:50 +0700

    chage flie and floder

---
+++ ignition/modules/Token.js
@@ -0,0 +1,15 @@
+
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+
+const TokenModule = buildModule("TokenModule", (m) => {
+
+  const token = m.contract("Token");
+
+
+  return { token };
+
+});
+
+
+module.exports = TokenModule;
